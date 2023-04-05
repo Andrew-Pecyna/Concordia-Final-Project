07bb405dc7c858aeb1c37824b0806a011ba28ddc
Andrew Pecyna <pecyna.andrew@gmail.com>
2023-04-05 10:47:16 -0700

    cloudinary setup and forum posts working

--- client/src/components/Forum.js
+++ client/src/components/Forum.js
@@ -1,28 +1,60 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../UserContext";
 import styled from "styled-components";
 import NavBar from "./NavBar";
-import RegisterButton from "./RegisterButton";
+import SideBarButton from "./SideBarButton";
+import MakePost from "./MakePost";
 
 const Forum = () => {
-    // Context to check if user has account/access to forum - to do
-    // const {currentUser, setCurrentUser} = useContext(UserContext)
+    const {currentUser, setCurrentUser} = useContext(UserContext)
+    const [forumPosts, setForumPosts] = useState([])
+    const [fetchSwitch, setFetchSwitch] = useState(false)
+
+    useEffect(() => {
+        const getForumPosts = async () => {
+            try{
+                const forumResponse = await fetch(`/api/get-forum`, { method: "GET" });
+                const parsedData = await forumResponse.json();
+                const forumData = parsedData.data
+
+                console.log("getForum data is : " + forumData)
+                setForumPosts(forumData)
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getForumPosts()
+
+    }, [fetchSwitch])
 
     return(
+        forumPosts === 0 ? <p>Loading...</p> :
         <>
         <NavBar />
         <MainContainer>
             <SideBar>
-                <div>
-                    <RegisterButton />
-                </div>
-                <RegisterBox>
-                    <p>Create an account to join the conversation and start adding to your bird collection! </p>
-                </RegisterBox>
+                {!currentUser && (
+                    <RegisterBox>
+                        <p>Create an account to join the conversation and start adding to your bird collection! </p>
+                    </RegisterBox>
+                )}
+                <ButtonBox>
+                    <SideBarButton />
+                </ButtonBox>
             </SideBar>
             <FeedWrapper>
                 <FeedContainer>
-                    <p>FORUM</p>
+                    {currentUser && <MakePost fetchSwitch={fetchSwitch} setFetchSwitch={setFetchSwitch} />}
+                    {forumPosts.map((each) => {
+                        return (
+                            <SinglePost key={each._id}>
+                                <p>{each.author}</p>
+                                <p>{each.text}</p>
+                                <Img src={each.image} />
+                            </SinglePost>
+                        )
+                    })}
                 </FeedContainer>
             </FeedWrapper>
         </MainContainer>
@@ -34,41 +66,59 @@ const Forum = () => {
 
 const MainContainer = styled.div`
 display: flex;
-height: 100vh;
+height: 82vh;
+min-width: 917px;
 `
 
 const FeedWrapper = styled.div`
 display: flex;
 justify-content: center;
 width: 60%;
-background-color: lightskyblue;
+background-color: whitesmoke;
 `
 
 const FeedContainer = styled.div`
+display: flex;
+flex-direction: column;
+row-gap: 15px;
 background-color: gainsboro;
-width: 800px;
-/* width: 70%; */
+margin: 30px 50px 0px 50px;
+padding: 0px 30px;
+width: 700px;
 min-width: 550px;
+overflow: scroll;
 `
 
 const SideBar = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-row-gap: 40px;
-background-color: steelblue;
+row-gap: 10px;
 width: 20%;
 min-width: 300px;
+border-right: 2px solid #020B4A;
+`
+
+const ButtonBox = styled.div`
 `
 
 const RegisterBox = styled.div`
-color: white;
+color: #020B4A;
 font-size: 17px;
 font-family: 'Helvetica', Arial, Helvetica, sans-serif;
-margin: 0px 40px;
-padding: 25px 0px;
+margin: 40px 40px 0px 40px;
+padding: 25px 0px 25px 0px;
 border-bottom: 2px solid gainsboro;
 border-top: 2px solid gainsboro;
+`
+
+const Img = styled.img`
+width: 200px;
+`
+
+const SinglePost = styled.div`
+border: 2px solid black;
+background-color: white;
 `
 
 export default Forum;
--- server/routes/birdRoutes.js
+++ server/routes/birdRoutes.js
@@ -1,11 +1,14 @@
 // Require packages
 const router = require("express").Router();
 
-const { getBirds, getUser, addUser } = require("../handlers/birdHandlers")
+const { getBirds, getUser, addUser, forumPost, getForum } = require("../handlers/birdHandlers")
 
 router.get('/birds', getBirds);
 
 router.get('/api/get-user/:email', getUser);
 router.post('/api/post-user', addUser);
+
+router.post('/api/forum-post', forumPost);
+router.get('/api/get-forum', getForum);
 
 module.exports = router;
